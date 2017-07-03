7e3c2251356767f07ab2abccf36c6b21a352305c
Minh Trang <matrang@hotmail.com>
2017-07-02 18:44:18 -0700

    Adding script to move par2 sets that are not recoverable

---
+++ Par2Repair/Par2Move.ps1
@@ -0,0 +1,15 @@
+﻿$a = Get-Content Par2RepairUnable.txt
+mkdir temp
+
+foreach($file in $a){
+    if($file -match 'vol\d+\+\d+'){
+        $b = [System.IO.Path]::GetFileNameWithoutExtension([System.IO.Path]::GetFileNameWithoutExtension($file))
+        Move-Item "$b*" temp
+    }
+    else{
+        $b = [System.IO.Path]::GetFileNameWithoutExtension($file)
+        Move-Item "$b*" temp
+    }
+    Write-Output $b >> Par2Move.txt
+    
+}
