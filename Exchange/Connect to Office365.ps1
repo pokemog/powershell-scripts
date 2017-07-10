8e3a1f3b61fd83020659340bf99c93de9c493c8e
Minh Trang <mtrang@extron.com>
2017-07-10 15:51:52 -0700

    Adding Office365 script

---
+++ Exchange/Connect to Office365.ps1
@@ -0,0 +1,4 @@
+﻿$credential = Get-Credential
+Connect-MsolService -Credential $credential
+$ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $ExchangeSession
