614e1dd4e46dc54533fb582528ee24fc985516e2
Minh Trang <mtrang@extron.com>
2017-08-01 12:03:47 -0700

    Changing formatting and not using alias

--- Exchange/CreateRooms.ps1
+++ Exchange/CreateRooms.ps1
@@ -1,19 +1,18 @@
 ﻿# Create rooms
-for ($i=1; $i -le 100; $i++)
-{
+for ($i = 1; $i -le 100; $i++) {
     New-Mailbox -Name "ExchangeRoom$i" -DisplayName "Exchange Room $i" -Room
 }
 
 # Set Calendar-Processing for all rooms to auto accept if available and to not delete Subject, Organizer and Body
-Get-MailBox | Where {$_.ResourceType -eq "Room"} | Set-CalendarProcessing -AutomateProcessing:AutoAccept -DeleteSubject $False -AddOrganizerToSubject $False -DeleteComment $False
+Get-MailBox | Where-Object {$_.ResourceType -eq "Room"} | Set-CalendarProcessing -AutomateProcessing:AutoAccept -DeleteSubject $False -AddOrganizerToSubject $False -DeleteComment $False
 
 # Set reviewer access to a user for all rooms
-Get-Mailbox | Where {$_.ResourceType -eq "Room"} | Add-MailboxFolderPermission -Identity ConfRoom1@extrondev.com:\calendar -User gveuser@extrondev.com -AccessRights Reviewer
+Get-Mailbox | Where-Object {$_.ResourceType -eq "Room"} | Add-MailboxFolderPermission -Identity ConfRoom1@extrondev.com:\calendar -User gveuser@extrondev.com -AccessRights Reviewer
 
-$mailboxes = Get-Mailbox | where {$_.ResourceType -eq "Room"}
+$mailboxes = Get-Mailbox | Where-Object {$_.ResourceType -eq "Room"}
 
-$mailboxes | foreach {
-    $user=$_.Alias
-    $path=$user+”:\Calendar”
+$mailboxes | ForEach-Object {
+    $user = $_.Alias
+    $path = $user + ”:\Calendar”
     Add-MailboxFolderPermission –Identity $path -User gveeng@extrondev.com -AccessRights Reviewer
 }
