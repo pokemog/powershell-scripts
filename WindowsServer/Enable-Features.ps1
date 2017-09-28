c335c3e26f634d9e7f8184600337eda752ecdff9
Minh Trang <mtrang@extron.com>
2017-09-28 15:45:38 -0700

    Adding Windows Optional Features to enable

---
+++ WindowsServer/Enable-Features.ps1
@@ -0,0 +1,52 @@
+IIS-WebServerRole
+IIS-WebServer
+
+# Common HTTP Features
+IIS-CommonHttpFeatures
+IIS-DefaultDocument
+IIS-DirectoryBrowsing
+IIS-HttpErrors
+IIS-StaticContent
+
+# Health and Diagnostics
+IIS-HealthAndDiagnostics
+IIS-HttpLogging
+
+# Performance
+IIS-Performance
+IIS-HttpCompressionStatic
+
+# Security
+IIS-Security
+IIS-RequestFiltering
+IIS-WindowsAuthentication
+
+# Application Development
+IIS-ApplicationDevelopment
+IIS-NetFxExtensibility45
+IIS-ASP
+IIS-ASPNET45
+IIS-ISAPIExtensions
+IIS-ISAPIFilter
+
+# Management Tools
+IIS-WebServerManagementTools
+IIS-ManagementConsole
+IIS-ManagementScriptingTools
+IIS-ManagementService
+
+# .NET Framework 4.6 Features
+NetFx4
+NetFx4Extended-ASPNET45
+WCF-HTTP-Activation45 
+WCF-TCP-PortSharing45 
+
+Windows-Identity-Foundation
+
+# Windows Process Activation Service
+WAS-ProcessModel
+WAS-ConfigurationAPI
+WAS-WindowsActivationService
+
+# WOW64 support
+ServerCore-WOW64
