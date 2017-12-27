0af6ce9bd584511c29b603079db55e61f6f153f9
Minh Trang <mtrang@extron.com>
2017-12-27 09:49:42 -0800

    Adding more script to remove docker

--- Docker/Remove-Docker.ps1
+++ Docker/Remove-Docker.ps1
@@ -21,3 +21,23 @@ Get-Item ‘HKLM:\software\Docker Inc.’ | Remove-Item -Recurse -Force
 Get-ItemProperty HKCU:\software\microsoft\windows\currentversion\Run -name “Docker for Windows” | Remove-Item -Recurse -Force
 #Get-ItemProperty HKCU:\software\microsoft\windows\currentversion\UFH\SHC | ForEach-Object {Get-ItemProperty $.PSPath} | Where-Object { $.ToString().Contains(“Docker for Windows.exe”) } | Remove-Item -Recurse -Force $.PSPath
 #Get-ItemProperty HKCU:\software\microsoft\windows\currentversion\UFH\SHC | Where-Object { $(Get-ItemPropertyValue $) -Contains “Docker” }
+
+
+# https://forums.docker.com/t/cannot-uninstall-docker-for-windows/15661/11
+'C:\Program Files\Docker\Docker\resources\MobyLinux.ps1' -Destroy
+$service = Get-WmiObject -Class Win32_Service -Filter "Name='com.docker.service'"
+$service.StopService()
+$service.Delete()
+Start-Sleep -s 5
+Remove-Item -Recurse -Force "~/AppData/Local/Docker"
+Remove-Item -Recurse -Force "~/AppData/Roaming/Docker"
+takeown.exe /F "C:\ProgramData\Docker" /R /A /D Y
+icacls "C:\ProgramData\Docker" /T /C /grant Administrators:F
+Remove-Item -Recurse -Force "C:\ProgramData\Docker"
+Remove-Item -Recurse -Force "C:\Program Files\Docker"
+Remove-Item -Recurse -Force "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Docker"
+Remove-Item -Force "C:\Users\Public\Desktop\Docker for Windows.lnk"
+Get-ChildItem hklm:\software\microsoft\windows\currentversion\uninstall | ForEach-Object {Get-ItemProperty $_.PSPath} | Where-Object { $_.DisplayName -eq "Docker" } | Remove-Item -Recurse -Force
+Get-ChildItem hklm:\software\classes\installer\products | ForEach-Object {Get-ItemProperty $_.pspath} | Where-Object { $_.ProductName -eq "Docker" } | Remove-Item -Recurse -Force
+Get-Item 'HKLM:\software\Docker Inc.' | Remove-Item -Recurse -Force
+Get-ItemProperty -path HKCU:\software\microsoft\windows\currentversion\Run -name "Docker for Windows" | Remove-Item -Recurse -Force
