559b7f0b49132281bb2bcea1470f878123fd50b4
Minh Trang <mtrang@extron.com>
2018-10-01 16:59:17 -0700

    Adding functions for license and connection strings

--- Misc/Update-Configuration.ps1
+++ Misc/Update-Configuration.ps1
@@ -1,5 +1,29 @@
-$Path = 'C:\Users\matra\source\repos\gve2\Build\configurations'
-$File = 'Settings.config' #, 'GVE.license.config', 'connectionStrings.config'
+# Updates GVE's Settings.config, connectionStrings, and GVE.license.config
+
+[CmdletBinding(PositionalBinding = $false)]
+Param (
+    # Path to GVE, defaults to GVE's default location
+    [Parameter(Mandatory = $false)]
+    [string] $GvePath = 'C:\inetpub\wwwroot\GVE\bin',
+    # Deploy Server Name
+    [Parameter(Mandatory = $true)]
+    [string] $GveServerIp,
+    # SQL Server Host
+    [Parameter(Mandatory = $true)]
+    [string] $DbServerIp,
+    # GVE Database Name
+    [Parameter(Mandatory = $true)]
+    [string] $DbName,
+    # SQL Server Username
+    [Parameter(Mandatory = $false)]
+    [String] $DbUsername = 'sa',
+    # SQL Server Password
+    [Parameter(Mandatory = $true)]
+    [string] $DbPassword,
+    # GVE Registration Key
+    [Parameter(Mandatory = $true)]
+    [string] $RegKey
+)
 
 function UpdateSettings {
     param (
@@ -9,36 +33,108 @@ function UpdateSettings {
         $DbName,
         $DbUsername,
         $DbPassword,
-        $RegKey
+        $RegistrationKey
     )
 
+    $Settings = @{
+        'ServerIPAddress'         = $ServerIp;
+        'DatabaseServerIP'        = $DbServerIp;
+        'DatabaseName'            = $DbName;
+        'DatabaseLoginName'       = $DbUsername;
+        'DatabasePassword'        = $DbPassword;
+        'ExistingRegistrationKey' = $RegistrationKey;
+        'ApplicationRootPath'     = "http://$($ServerIP)/GVE/"
+    }
+
     [Xml] $Xml = Get-Content $PathToSettingsFile
-    $OldChild = $Xml.appSettings.add | Where-Object {$_.key -eq 'ServerIPAddress'}
-    $NewChild = $OldChild.Clone()
-    $NewChild.value = $ServerIp
-    Write-Host "Old Child is: " $OldChild.value
-    Write-Host "New Child is: " $NewChild.value
-    $Xml.appSettings.ReplaceChild($NewChild, $OldChild)
-    # $keyIdx = $Xml.appSettings.add.key.IndexOf('ServerIPAddress')
-    # $Xml.appSettings.add.value[$keyIdx] = $ServerIp
-    # $keyIdx = $Xml.appSettings.add.key.IndexOf('DBServerHostname')
-    # $Xml.appSettings.add.value[$keyIdx] = $DbServerIp
-    # $keyIdx = $Xml.appSettings.add.key.IndexOf('DatabaseName')
-    # $Xml.appSettings.add.value[$keyIdx] = $DbName
-    # $keyIdx = $Xml.appSettings.add.key.IndexOf('DatabaseLoginName')
-    # $Xml.appSettings.add.value[$keyIdx] = $DbUsername
-    # $keyIdx = $Xml.appSettings.add.key.IndexOf('DatabasePassword')
-    # $Xml.appSettings.add.value[$keyIdx] = $DbPassword
-    # $keyIdx = $Xml.appSettings.add.key.IndexOf('ExistingRegistrationKey')
-    # $Xml.appSettings.add.value[$keyIdx] = $RegKey
-    # $keyIdx = $Xml.appSettings.add.key.IndexOf('ApplicationRootPath')
-    # $Xml.appSettings.add.value[$keyIdx] = "http://$ServerIp/GVE"
 
-    # Set-Location 'C:\Users\matra\source\repos\gve2\Build\configurations'
+    foreach ($Setting in $Settings.GetEnumerator()) {
+        $OldChild = $Xml.appSettings.add | Where-Object {$_.key -eq $Setting.Key}
+        $NewChild = $OldChild.Clone()
 
-    Write-Host = Get-Location
-    $Xml.Save($PathToSettingsFile)
+        if ($Setting.Key -eq 'DatabasePassword') {
+            $Setting.Value = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes(":$($Setting.Value)"))
+        }
+
+        $NewChild.value = $Setting.Value
+        $Xml.appSettings.ReplaceChild($NewChild, $OldChild)
+    }
+
+    #$Xml.Save($PathToSettingsFile)
 }
 
-$FilePath = Join-Path -Path $Path -ChildPath $File
-UpdateSettings $FilePath '192.168.1.1' '192.168.1.2' 'gve' 'sa' 'extron' 'Registration Key'
+function UpdateConnectionStrings {
+    param (
+        $PathToSettingsFile,
+        $DbServerIp,
+        $DbName,
+        $DbUsername,
+        $DbPassword
+    )
+
+    $DbContexts = 'gveconn', 'gveconnDbContext', 'GveModel'
+    
+    [Xml] $Xml = Get-Content $PathToSettingsFile
+    
+    foreach ($DbContext in $DbContexts) {
+        $OldChild = $Xml.connectionStrings.add | Where-Object {$_.name -eq $DbContext}
+        $NewChild = $OldChild.Clone()
+        if ($DbContext -eq 'GveModel') {
+            $SqlBuilder = New-Object System.Data.EntityClient.EntityConnectionStringBuilder -ArgumentList $OldChild.connectionString
+            $SqlBuilder["metadata"] = "res://*/EF.GveModel.csdl|res://*/EF.GveModel.ssdl|res://*/EF.GveModel.msl"
+            $SqlBuilder["provider"] = 'System.Data.SqlClient'
+            $SqlBuilder["Data Source"] = $DbServerIp
+            $SqlBuilder["Initial Catalog"] = $DbName
+            $SqlBuilder["User ID"] = $DbUsername
+            $SqlBuilder["Password"] = $DbPassword
+
+            $NewChild.connectionString = $SqlBuilder.ToString()
+            $Xml.connectionStrings.ReplaceChild($NewChild, $OldChild)
+        }
+        else {
+            $SqlBuilder = New-Object System.Data.SqlClient.SqlConnectionStringBuilder -ArgumentList $OldChild.connectionString
+            $SqlBuilder["Data Source"] = $DbServerIp
+            $SqlBuilder["Initial Catalog"] = $DbName
+            $SqlBuilder["User ID"] = $DbUsername
+            $SqlBuilder["Password"] = $DbPassword
+
+            $NewChild.connectionString = $SqlBuilder.ToString()
+            $Xml.connectionStrings.ReplaceChild($NewChild, $OldChild)
+        }
+    }
+}
+function UpdateLicense {
+    param (
+        $PathToSettingsFile,
+        $ServerIp,
+        $RegistrationKey
+    )
+    
+    [Xml] $Xml = Get-Content $PathToSettingsFile
+    $Xml.RegistrationInfo.ExistingRegistrationKey = $RegistrationKey
+    $Xml.RegistrationInfo.IPAddress = $ServerIp
+    #$Xml.Save($PathToSettingsFile)
+}
+
+$SettingsFiles = 'Settings.config', 'GVE.license.config', 'connectionStrings.config'
+
+foreach ($SettingsFile in $SettingsFiles) {
+    switch ($SettingsFile) {
+        'Settings.config' {
+            $FilePath = Join-Path -Path $GvePath -ChildPath $SettingsFile
+            UpdateSettings $FilePath $GveServerIp $DbServerIp $DbName $DbUsername $DbPassword $RegKey
+            break
+        }
+        'connectionStrings.config' {
+            #C:\Windows\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis -pd "connectionStrings" -app /GVE
+            $FilePath = Join-Path -Path $GvePath -ChildPath $SettingsFile
+            UpdateConnectionStrings $FilePath $DbServerIp $DbName $DbUsername $DbPassword
+        }
+        'GVE.license.config' {
+            $FilePath = Join-Path -Path $GvePath -ChildPath $SettingsFile
+            UpdateLicense $FilePath $GveServerIp $RegKey
+            break
+        }
+        Default {}
+    }    
+}
